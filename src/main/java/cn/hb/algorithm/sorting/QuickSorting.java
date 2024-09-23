04530ea0fee85745bb69f1813ba8eed037d072b1
huangbin <IAM_970921415@csair.com>
2024-09-23 14:51:12 +0800

    feat：修复快速排序第二种实现方法

--- src/main/java/cn/hb/algorithm/sorting/QuickSorting.java
+++ src/main/java/cn/hb/algorithm/sorting/QuickSorting.java
@@ -9,7 +9,19 @@ import static cn.hb.utils.RandomUtil.RANDOM;
  *
  * @author abin
  * @date 2024 /9/22 19:13
- * @description
+ * @description ### 工作原理
+ *
+ * 快速排序使用分治法（Divide and Conquer）来对数组进行排序。其基本步骤如下：
+ *
+ * 1. **选择基准值**：从数组中选择一个元素作为基准值（pivot）。
+ * 2. **分区操作**：将数组分为两部分，一部分的所有元素都小于基准值，另一部分的所有元素都大于基准值。
+ * 3. **递归排序**：对这两部分分别递归地进行快速排序。
+ *
+ * ### 时间复杂度
+ *
+ * - 最好情况：(O(nlog n))
+ * - 平均情况：(O(nlog n))
+ * - 最坏情况：(O(n^2))
  */
 public class QuickSorting {
     /**
@@ -18,9 +30,13 @@ public class QuickSorting {
     private static int rounds = 1;
 
     public static void main(String[] args) {
-        int[] arr = {880900,918767,427094,223627,665730,924322};
-        System.out.println(Arrays.toString(arr));
-        quickSorting(arr, 0, arr.length - 1);
+//        int[] arr = {880900,918767,427094,223627,665730,924322};
+//        System.out.println(Arrays.toString(arr));
+//        quickSorting(arr, 0, arr.length - 1);
+
+        int[] arr1 = {880900,918767,427094,223627,665730,924322};
+        quickSortingTwo(arr1, 0, arr1.length - 1);
+        System.out.println(Arrays.toString(arr1));
     }
 
     /**
@@ -69,21 +85,40 @@ public class QuickSorting {
      * @param right 正确
      */
     public static void quickSortingTwo(int[] arr, int left, int right) {
+        // 基本情况：当子数组长度小于等于1时直接返回
         if (left >= right) {
             return;
         }
+
+        // 初始化指针和基准值
         int i = left;
         int j = right;
         int pivot = arr[left];
+
+        // 双指针交换元素
         while (i < j) {
+            // 从右向左找到小于基准值的元素
             while (i < j && arr[j] >= pivot) {
                 j--;
             }
+            // 从左向右找到大于基准值的元素
             while (i < j && arr[i] <= pivot) {
                 i++;
             }
-            if (i <j) {}
+
+            // 如果 i < j，则交换 arr[i] 和 arr[j]
+            if (i < j) {
+                swap(arr, i, j);
+            }
         }
+
+        // 将基准值放到正确位置
+        arr[left] = arr[i];
+        arr[i] = pivot;
+
+        // 递归调用快速排序
+        quickSortingTwo(arr, left, i - 1);
+        quickSortingTwo(arr, i + 1, right);
     }
 
     public static void swap(int[] arr, int i, int j) {
